3c5114fc02613250b22f9d8693b9b98f135670b9
Muskan Kumari <muskan@example.com>
2025-08-05 23:27:20 +0530

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-//import Testimonials from "./sections/Testimonials";
-//import Footer from "./sections/Footer";
-//import Contact from "./sections/Contact";
+import Testimonials from "./sections/Testimonials";
+import Footer from "./sections/Footer";
+import Contact from "./sections/Contact";
 import TechStack from "./sections/TechStack";
 import Experience from "./sections/Experience";
 import Hero from "./sections/Hero";
@@ -18,6 +18,9 @@ const App = () => (
     <FeatureCards />
     <Experience />
     <TechStack />
+    <Testimonials />
+    <Contact />
+    <Footer />
   </>
 );
 
