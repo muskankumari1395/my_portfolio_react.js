f14c6a47d707d345f0eab3c00a2566f5ead447d5
Muskan Kumari <muskangen1395@gmail.com>
2025-08-06 14:15:30 +0530

    #

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
       <div className="inner">
         <a href="#hero" className="logo">
-          Muskan JSM
+          Khushi
         </a>
 
         <nav className="desktop">
