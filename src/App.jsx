5cc28a119751bc06613d91623650baa4decf400e
Muskan Kumari <muskan@example.com>
2025-08-06 14:39:08 +0530

    #

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TechStack from "./sections/TechStack";
 import Experience from "./sections/Experience";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
+import AboutMe from "./sections/AboutMe";
 import LogoShowcase from "./components/LogoShowcase";
 import FeatureCards from "./sections/FeatureCards";
 import NavBar from "./components/NavBar";
@@ -18,6 +19,7 @@ const App = () => (
     <NavBar />
     <Hero />
     <ShowcaseSection />
+    <AboutMe />
     <LogoShowcase />
     <FeatureCards />
     <Experience />
